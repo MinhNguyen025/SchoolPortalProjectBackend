590f5d107bac76b9e2e9c14c1be1f2505be17759
MinhWuyn <nguyenduyminh0205@gmail.com>
2024-09-03 20:54:21 +0700

    almost done!

---
+++ src/main/java/com/timetable/dao/CourseDao.java
@@ -0,0 +1,18 @@
+package com.timetable.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.timetable.entity.Course;
+import com.timetable.entity.Grade;
+
+@Repository
+public interface CourseDao extends JpaRepository<Course, Integer> {
+	
+	List<Course> findByGradeAndStatus(Grade grade, String status);
+	
+	List<Course> findByStatus(String status);											
+
+}
